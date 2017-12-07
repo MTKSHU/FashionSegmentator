d027d25f302144147a1b2620c0420d9933119e3f
Christian <joppi94@gmail.com>
2017-12-07 16:46:42 +0100

    change UI Demo

--- demo_api/js/submit.js
+++ demo_api/js/submit.js
@@ -49,6 +49,7 @@ function send_data(){
     formData.append('urls', $('#urls').val());
     formData.append('zip_result', false);
     formData.append('heavy', true);
+    $("#clear_btn").click();
     var result = 
         $.ajax({
         url: base_url + "images/",
@@ -64,101 +65,92 @@ function send_data(){
             $("#box_result").prop("hidden",false);
             var pic_path = base_url + data['pic'].split('/')[5] + '/' + data['pic'].split('/')[6] + '/' +data['pic'].split('/')[7];
             $("#res_img").attr('src',pic_path);
-            $("li").remove();
-
+            //$("li").remove();
+            $(".child_circ").remove();
+            $(".child").remove();
             var offs = 0;
             for(var j=0;j<data['labels'].length;j++){
                 if(parseInt(data['labels'][j]['num']) < 3)
                     offs++;
                 else{
-                    add_label_info(j-offs,data['labels'][j]);  
+                    add_circ('#0088cc',j-offs,data['bounding_box'][j-offs]);  
                 }
             }
 
 
-            
+            /*
             $('.label').on('click',function(){
-                var ind = $(this).attr('id');
+                var ind = parseInt($(this).attr('id'));
                 $(".label").removeClass('selected');
                 $(this).addClass('selected');
                 $(".child").remove();
-                add_rect("red",data['bounding_box'][ind]);
+                add_rect("#0088cc",data['bounding_box'][ind],data['labels'][ind+offs]['label']);
+                $(".child_circ").prop("hidden",true);
             });
-            $('.label').first().click();         
+        */
+
+            $(".child_circ").on('click',function(){
+                var ind = parseInt($(this).attr('id'));
+                $(".child").remove();
+                add_rect("#0088cc",data['bounding_box'][ind],data['labels'][ind+offs]['label']);
+                $(".child_circ").prop("hidden",false);
+                $(this).prop("hidden",true);
+            });
             $("#waitbar").addClass('hidden');
             on_load(false);
-            $("#clear_btn").click();
             return false;
         },
         error: function (XMLHttpRequest, textStatus, errorThrown){
             console.log(XMLHttpRequest)
             $("#waitbar").addClass('hidden');
             on_load(false);
-            $("#clear_btn").click();
             alert("Elaboration Error!!");
             return false;
 
         }
     });
+
 }
 
 
-var add_rect = function(color, rect) {
+var add_rect = function(color, rect,text) {
     var $container = $("#container");
-    $('<div class="child" />')
-    .appendTo($container)
-    .css("left", (rect['min_x']-5) + "px")
-    .css("top", (rect['min_y']-5) + "px")
-    .css("width", (rect['max_x']-rect['min_x']+10)+"px")
-    .css("height", (rect['max_y']-rect['min_y']+10)+"px")
-    .css("border", "3px solid " + color);
+    $('<span class="label label-primary">'+text+'</span>').appendTo(
+        $('<div class="child" />')
+        .appendTo($container)
+        .css("left", (rect['min_x']-5) + "px")
+        .css("top", (rect['min_y']-5) + "px")
+        .css("width", (rect['max_x']-rect['min_x']+10)+"px")
+        .css("height", (rect['max_y']-rect['min_y']+10)+"px")
+        .css("border", "3px solid " + color))
+    .css("position","relative")
+    .css("left", "-3px")
+    .css("top", "-7px");
+
+
 
 };
-
-var add_label_info = function(index,label_text){
-    label_text = label_text['label'];
-    var $labels_container = $("#labels");
-    $("<li><i class='fa fa-check' /><span class='label' id='"+index+"'>"+label_text+"</span></li>").appendTo($labels_container);
-}   
-
-
-var getObjects = function(obj, key, val) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getObjects(obj[i], key, val));
-        } else if (i == key && obj[key] == val) {
-            objects.push(obj);
-        }
-    }
-    return objects;
+var add_circ = function(color,index, rect){
+    var $container = $("#container");
+    $('<div class="child_circ" id="'+index+'"/>')
+    .appendTo($container)
+    .css("left", ((rect['min_x']+ rect['max_x'])/2)+ "px")
+    .css("top", ((rect['min_y']+ rect['max_y'])/2) + "px")
+    .css("background-color", color)
+    .css("cursor","pointer")
+    .css("border", "3px solid " + color);
 }
 
-
-var max_dress = function(arr){
-    max = 0;
-    el_max = -1;
-    for(var j=0;j<arr.length;j++)
-        if(arr[j]!=undefined)
-            if(arr[j]['score'] > max)
-            {
-                max = arr[j]['score'];
-                el_max = arr[j];
-            }
-    return el_max;
-
-}
 
 var on_load  = function(load){
     
     $('input[type=file]').prop("disabled",load);
     $("#urls").prop("disabled",load);
-    $("#clear_btn").prop("disabled",load);
     $("#send_data").prop("disabled",load);
     
     if(load)
         $(".main").css('opacity',0.05)
-    else
+    else{
         $(".main").css('opacity',1)
+    }
 }
