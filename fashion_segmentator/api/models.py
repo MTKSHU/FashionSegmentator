5632e47049376c78c8ca09e468ab9d17d952c600
Christian <joppi94@gmail.com>
2018-01-31 17:07:35 +0100

    change name limit len

--- fashion_segmentator/api/models.py
+++ fashion_segmentator/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class ImageUploaded (models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=400)
     pic = models.ImageField(null=True,blank=True,upload_to=r"pics/")
     urls = models.CharField(null=True,blank=True,max_length=500)
     time = models.DateTimeField(editable=False)
